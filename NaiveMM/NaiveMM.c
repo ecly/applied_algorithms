9f46e7c34da21c4e4a2fa71597ea16f3ccf4b335
ecly <ecly@mailbox.org>
2017-08-29 19:22:47 +0200

    optimized implementation to keep variables of one matrix in memory for all their calculations

--- NaiveMM/NaiveMM.c
+++ NaiveMM/NaiveMM.c
@@ -6,40 +6,35 @@ int* matrixFromFile(FILE *fp, int n){
 
     //ignore first comment line
     fscanf(fp, "%*[^\n]\n", NULL);
-
-    int i;
-    int j;
-    int lineCount = n*n;
-    int characterCount = 5;//number of chars on line
-    for(i = 0; i < lineCount; i++){
-            int col,row,val;
-            fscanf(fp, "%d,%d,%d", &col, &row, &val);
-            mat[col*n+row] = val;
-            fgetc(fp);//newline
-        }
+    for(int i = 0; i < n*n; i++){
+        int val;
+        fscanf(fp, "%*d,%*d,%d", &val);
+        mat[i] = val;
+    }
     return mat;
 }
 
 int multiplyMatrices(int* a, int* b, int n){
-    int i,j,k;
-    for(i = 0; i < n; i++){
-        for(j = 0; j < n; j++){
-            int val = 0;
-            for(k = 0; k < n; k++){
-                val = val + a[i*n+k]*b[k*n+j];
+    int sum;
+    int *mat = (int *)malloc(n*n * sizeof(int));
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            int x = a[i*n+j];
+            for(int k = 0; k < n; k++){
+                mat[k+i*n] += x * b[j*n+k];
             }
-           printf("%d ", val);
         }
     }
-    return 1;
+    for(int f = 0; f < n*n; f++)
+        printf("%d ", *mat++);
+
+    return 0;
 }
 
 int main(int argc, char *argv[]){
     int n = atoi(argv[1]);
-    FILE *fp1;
-    FILE *fp2;
-    fp1 = fopen(argv[2], "r");
-    fp2 = fopen(argv[3], "r");
+    FILE *fp1 = fopen(argv[2], "r");
+    FILE *fp2 = fopen(argv[3], "r");
     int* m1 = matrixFromFile(fp1, n);
     int* m2 = matrixFromFile(fp2, n);
     multiplyMatrices(m1, m2, n);
