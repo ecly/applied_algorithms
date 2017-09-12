3c0194ef4574399f1c1419d38eb75fb64713aa0d
ecly <ecly@mailbox.org>
2017-09-12 09:23:24 +0200

    initial add of anf assignment

---
+++ ANF/handout/ApproxSet.java
@@ -0,0 +1,36 @@
+/**
+ * Maintain a dynamic set under insertion, reporting the approximate number of distinct items inserted using a HyperLogLog counter.
+ * @author Rasmus Pagh
+ * @version 2016.11.21
+ */
+
+public class ApproxSet {
+
+	private static int logm = 10;
+	private static int m = 1<<logm;
+	private byte[] M = new byte[m];
+
+	public void add(Object x) {
+		int xh = x.hashCode();
+		if (xh!=0) {
+			int i = LinearHash.f(xh,logm);
+			byte val = (byte)ExpHash.hash(xh);
+			if (val>M[i]) M[i]=val;
+		}
+	}
+	
+	public int sizeEstimate() {
+		double wsum = 0;
+		int zerosum = 0;
+		for (int j=0; j<m; j++) {
+			wsum += Math.pow(2.0,-M[j]);
+			if (M[j]==0) zerosum++;
+		}
+		double Z = 1/wsum;
+		double estimate = m*m*Z*0.7213/(1 + 1.079/m);
+		if ((estimate < 2.5 * m) && (zerosum>0))
+			estimate = m * Math.log((double)m/zerosum);
+		return (int)estimate;
+	}
+
+}
