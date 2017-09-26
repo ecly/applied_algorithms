ef5892b481cb5b70bad01c385ba8aa1c94e230d2
ecly <ecly@mailbox.org>
2017-09-26 20:50:06 +0200

    finished most of inital setup prior to mst implementation

--- MST/MST.go
+++ MST/MST.go
@@ -2,16 +2,19 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 )
 
 // Globals
 var Seed int32
 var VertexAmount int32
 var VertexSeed []int32
+var Graph []Edge
 
 type Edge struct {
-	Y      int32
 	X      int32
+	Y      int32
 	Weight int32
 }
 
@@ -19,89 +22,109 @@ func MST(graph []int32) {
 	fmt.Println("Solved")
 }
 
-func generateComplete(seed int32, numOfVertices int32) []int32 {
-	complete := []int32{1}
+func generateComplete() []Edge {
+	// at least this big for starters
+	var graph []Edge
+	for i := int32(0); i < VertexAmount; i++ {
+		for j := i + 1; j < VertexAmount; j++ {
+			graph = append(graph, Edge{i, j, getEdgeWeight(i, j)})
+		}
+	}
+	return graph
+}
+
+func generateGrid(numX int32, numY int32) []Edge {
+	complete := []Edge{Edge{1, 2, 3}}
 	return complete
 }
 
-func generateGrid(seed int32, numX int32, numY int32) []int32 {
-	complete := []int32{1}
+func readGraph(filename string, numOfEdges int32) []Edge {
+	complete := []Edge{Edge{1, 2, 3}}
 	return complete
 }
 
-func readGraph(seed int32, filename string, numOfVertices int32, numOfEdges int32) []int32 {
-	complete := []int32{1}
-	return complete
-}
-
-const EDGE_MOD = 100000
+//var int32 EDGE_MOD := 100000
 
 func getEdgeWeight(v1 int32, v2 int32) int32 {
-	return xorshift32(VertexSeed[v1]^VertexSeed[v2]) % EDGE_MOD
+	return xorshift32(VertexSeed[v1]^VertexSeed[v2]) % int32(100000)
 }
 
 func xorshift32(seed int32) int32 {
 	ret := seed
-	ret ^= ret << 13
-	ret ^= ret >> 17
-	ret ^= ret << 5
+	ret ^= ret << uint32(13)
+	ret ^= ret >> uint32(17)
+	ret ^= ret << uint32(5)
 	return ret
 }
 
-func generateSeeds(seed int32) {
-	VertexSeed[0] = xorshift32(seed)
+func generateSeeds() {
+	VertexSeed = make([]int32, VertexAmount, VertexAmount)
+	VertexSeed[0] = xorshift32(Seed)
 	for i := int32(1); i < VertexAmount; i++ {
-		VertexSeed[i] = xorshift32(VertexSeed[i-1] ^ seed)
+		VertexSeed[i] = xorshift32(VertexSeed[i-1] ^ Seed)
 	}
 }
 
 func hashRand(inIndex int32) int32 {
-	const b = 0x5f375a86 //bunch of random bits
+	const b = int32(0x5f375a86) //bunch of random bits
 	for i := 0; i < 8; i++ {
 		inIndex = (inIndex + 1) * ((inIndex >> 1) ^ b)
 	}
 	return inIndex
 }
 
-func mstToint32(mst []Edge, mstsize int32) int32 {
+func mstToInt(mst []Edge) int32 {
 	total := int32(0)
-	for i := int32(0); i < mstsize; i++ {
+	for i := 0; i < len(mst); i++ {
 		total += hashRand(mst[i].Weight)
 	}
 	return total
 }
 
-func createEdge(v1 int32, v2 int32) Edge {
-	return Edge{v1, v2, getEdgeWeight(v1, v2)}
-}
-
 func main() {
-	//args := os.Args[1:]
-	//var graph []int32
-	/*
-		switch len(args) {
-		case 2:
-			Seed, _ := strconv.Atoi(args[0])
-			VertexAmount, _ := strconv.Atoi(args[1])
-			graph = generateComplete(seed, numOfVertices)
-		case 3:
-			Seed, _ := strconv.Atoi(args[0])
-			numX, _ := strconv.Atoi(args[1])
-			numY, _ := strconv.Atoi(args[2])
-			NumOfVertices = numX * numY
-			graph = generateGrid(seed, numX, numY)
-		case 4:
-			Seed, _ := strconv.Atoi(args[0])
-			filename := args[1]
-			VertexAmount, _ := strconv.Atoi(args[2])
-			numOfEdges, _ := strconv.Atoi(args[3])
-			graph = readGraph(seed, filename, numOfVertices, numOfEdges)
-		}
-	*/
+	fmt.Println("nice", int32(0x5f37a86))
+	args := os.Args[1:]
+	var graph []Edge
+	switch len(args) {
+	case 2:
+		seed, _ := strconv.Atoi(args[0])
+		Seed = int32(seed)
+		vertexAmount, _ := strconv.Atoi(args[1])
+		VertexAmount = int32(vertexAmount)
+		fmt.Printf("Seed %d, Amount %d \n", Seed, VertexAmount)
 
-	VertexSeed = []int32{899619065, 857390176, -1009232974}
-	Seed = 1597463007
-	Graph := []Edge{createEdge(0, 1), createEdge(0, 2), createEdge(1, 2)}
-	fmt.Println(mstToint32(Graph, 3))
-	//MST(graph)
+		generateSeeds()
+		for i := 0; i < len(VertexSeed); i++ {
+			fmt.Println(VertexSeed[i])
+		}
+		graph = generateComplete()
+		for i := 0; i < len(graph); i++ {
+			x := graph[i]
+			fmt.Printf("X:%d, Y:%d, Weight: %d\n", x.X, x.Y, x.Weight)
+		}
+	case 3:
+		seed, _ := strconv.Atoi(args[0])
+		Seed = int32(seed)
+
+		numX, _ := strconv.Atoi(args[1])
+		numY, _ := strconv.Atoi(args[2])
+
+		X := int32(numX)
+		Y := int32(numY)
+		VertexAmount = X * Y
+
+		generateSeeds()
+		graph = generateGrid(X, Y)
+	case 4:
+		seed, _ := strconv.Atoi(args[0])
+		Seed = int32(seed)
+		filename := args[1]
+		vertexAmount, _ := strconv.Atoi(args[2])
+		numOfEdges, _ := strconv.Atoi(args[3])
+		VertexAmount = int32(vertexAmount)
+		graph = readGraph(filename, int32(numOfEdges))
+	}
+
+	//mst := MST(graph)
+	fmt.Println(mstToInt(graph))
 }
