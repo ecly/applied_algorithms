6fbf3047332baaf03b5e0a06a8a48aa08eb47abc
ecly <ecly@mailbox.org>
2017-10-29 19:40:28 +0100

    tweaked constants and small optimization for permutation generation

--- CorrelatedPair/minhash.go
+++ CorrelatedPair/minhash.go
@@ -7,14 +7,12 @@ import (
 	"math/bits"
 	"math/rand"
 	"os"
-	"runtime/debug"
 	"strconv"
 	"strings"
-	"time"
 )
 
 // the min similarity we're looking for
-const THRESHOLD = 65
+const THRESHOLD = 70
 
 // the cutoff for a single uint64's similarity to be considered
 const CUTOFF = 15
@@ -22,7 +20,7 @@ const CUTOFF = 15
 // the number of bits each vector holds
 const AMOUNT_OF_BITS = 256
 
-const AMOUNT_OF_SUBBUCKETS = 15
+const AMOUNT_OF_SUBBUCKETS = 4
 
 // A BitVector represented as 4 uint64 with an additional
 // index indicating it's index in the original slice of BitVectors.
@@ -35,7 +33,7 @@ type BitVector256 struct {
 }
 
 // A Key used as key in a map
-type Key [AMOUNT_OF_SUBBUCKETS]int
+type Key [AMOUNT_OF_SUBBUCKETS]uint8
 
 // Returns the similarity of two BitVectors
 // meaning the number of bits set in both Vectors
@@ -51,12 +49,13 @@ func (b BitVector256) Compare(b1 BitVector256) int {
 	return similarity
 }
 
+// brute force in a cache efficient way
 func correlatedPair(vectors []BitVector256) (int, int) {
-	//fmt.Printf("Vector of size %d\n", len(vectors))
 	for i := 0; i < len(vectors); i++ {
+		vec := vectors[i]
 		for j := i + 1; j < len(vectors); j++ {
-			if vectors[i].Compare(vectors[j]) > THRESHOLD {
-				return vectors[i].index, vectors[j].index
+			if vec.Compare(vectors[j]) > THRESHOLD {
+				return vec.index, vectors[j].index
 			}
 		}
 	}
@@ -74,64 +73,75 @@ func compareInBuckets(buckets map[Key][]BitVector256) (int, int) {
 	return -1, -1
 }
 
-func findSetBit(permutation [AMOUNT_OF_BITS]uint, vector BitVector256) int {
-	for _, v := range permutation {
+func findSetBit(permutation [AMOUNT_OF_BITS]uint8, vector BitVector256) uint8 {
+	for i := 0; i < AMOUNT_OF_BITS; i++ {
+		v := permutation[i]
 		//fmt.Printf("V: %d\n", v)
 		switch v / 4 {
 		case 0:
 			if vector.a&(1<<(v%64)) != 0 {
-				return int(v)
+				return v
 			}
 		case 1:
 			if vector.b&(1<<(v%64)) != 0 {
-				return int(v)
+				return v
 			}
 		case 2:
 			if vector.c&(1<<(v%64)) != 0 {
-				return int(v)
+				return v
 			}
 		case 3:
 			if vector.d&(1<<(v%64)) != 0 {
-				return int(v)
+				return v
 			}
 		}
 	}
 	// this should never be the case with the input sake
 	// will however happen for BitVector256 with all 0's
-	return -1
+	return 0
 }
 
-func generatePermutation() [AMOUNT_OF_BITS]uint {
-	signed_perm := rand.Perm(AMOUNT_OF_BITS)
-	var permutation [AMOUNT_OF_BITS]uint
-	for i, v := range signed_perm {
-		permutation[i] = uint(v)
+// generate the numbers 0..255 as uint8
+func defaultPermutation() [AMOUNT_OF_BITS]uint8 {
+	var arr [AMOUNT_OF_BITS]uint8
+	for i := 0; i < AMOUNT_OF_BITS; i++ {
+		arr[i] = uint8(i)
 	}
-	return permutation
+	return arr
+}
+
+// permutates an array similarly to how golang's libraries do it
+func generatePermutation(slice [AMOUNT_OF_BITS]uint8) [AMOUNT_OF_BITS]uint8 {
+	for i := range slice {
+		j := rand.Intn(i + 1)
+		slice[i], slice[j] = slice[j], slice[i]
+	}
+	return slice
 }
 
 func groupInBuckets(vectors []BitVector256) map[Key][]BitVector256 {
-	rand.Seed(time.Now().UTC().UnixNano())
+	//rand.Seed(time.Now().UTC().UnixNano())
 	buckets := make(map[Key][]BitVector256, AMOUNT_OF_BITS)
-	var permutations [AMOUNT_OF_SUBBUCKETS][AMOUNT_OF_BITS]uint
+	var permutations [AMOUNT_OF_SUBBUCKETS][AMOUNT_OF_BITS]uint8
 
 	// generate all the permutations once
+	defaultPermutation := defaultPermutation()
 	for i := 0; i < AMOUNT_OF_SUBBUCKETS; i++ {
-		permutations[i] = generatePermutation()
+		permutations[i] = generatePermutation(defaultPermutation)
 	}
 	for _, v := range vectors {
-		var key [AMOUNT_OF_SUBBUCKETS]int
+		var key [AMOUNT_OF_SUBBUCKETS]uint8
 		// create the key as an array of size AMOUNT_OF_SUBBUCKETS
 		for j := 0; j < AMOUNT_OF_SUBBUCKETS; j++ {
 			key[j] = findSetBit(permutations[j], v)
 		}
-		//fmt.Printf("Key: %d, %d, %d\n", key.X, key.Y, key.Z)
 		buckets[key] = append(buckets[key], v)
 	}
 
 	return buckets
 }
 
+// recursively try until result is found
 func minHash(vectors []BitVector256) (int, int) {
 	buckets := groupInBuckets(vectors)
 	i1, i2 := compareInBuckets(buckets)
@@ -167,13 +177,7 @@ func readVectors(filename string, vectorAmount int) []BitVector256 {
 	return vectors
 }
 
-// Utility function
-func makeTimestamp() int64 {
-	return time.Now().UnixNano() / int64(time.Millisecond)
-}
-
 func main() {
-	debug.SetGCPercent(-1)
 	filename := os.Args[1]
 	//longAmount, _ := strconv.Atoi(os.Args[2])
 	vectorAmount, _ := strconv.Atoi(os.Args[3])
