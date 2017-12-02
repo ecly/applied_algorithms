686503db19683152f1ed981d343c55422a90ca08
ecly <ecly@mailbox.org>
2017-12-02 19:00:20 +0100

    added known/unknown invariant for tag accuracy

--- HMM_POS_Evaluation/evaluate.py
+++ HMM_POS_Evaluation/evaluate.py
@@ -61,3 +61,26 @@ def get_accuracy(s_data, g_data):
 
 accuracies = {lang:get_accuracy(data[lang][2], data[lang][3]) for lang in langs}
 print (accuracies)
+
+def get_accuracy_known_vs_unknown(s_data, g_data, lexicon):
+    """
+    Separates the accuracy for known and unknown words.
+    Known = exists in the training set.
+    """
+    correct_known = 0
+    correct_unknown = 0
+    total_known = 0
+    total_unknown = 0
+    for s_sent, g_sent in zip(s_data, g_data):
+        for (word, s_tag), (_, g_tag) in zip(s_sent, g_sent):
+            if word in lexicon:
+                total_known += 1
+                correct_known += int(s_tag == g_tag)
+            else:
+                total_unknown += 1
+                correct_unknown += int(s_tag == g_tag)
+    return correct_known/total_known, correct_unknown/total_unknown
+
+accuracies = {lang:get_accuracy(data[lang][2], data[lang][3]) for lang in langs}
+print("known vs unknown")
+print (accuracies)
