6295fb84ead09e021abd197d94aba4a3933291fd
ecly <ecly@mailbox.org>
2017-12-05 17:08:31 +0100

    removed HHM_POS as it went nowhere

--- HMM_POS_Evaluation/run.sh
+++
@@ -1,3 +0,0 @@
-for lang in cs de en fr hi; do 
-    ./tnt/tnt models/${lang} data/${lang}.test > data/${lang}.test.tagged
-done
--- HMM_POS_Evaluation/train.sh
+++
@@ -1,3 +0,0 @@
-for lang in cs de en fr hi; do 
-    ./tnt/tnt-para -o models/${lang} data/${lang}.train
-done
